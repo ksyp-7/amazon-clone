d9fbe212a1eb5690a15e142172dde093986af36d
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-31 17:30:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <Header />
       {/*Body */}
     </div>
