230394f4a95799a06e2d701c3c570effca5e5517
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-31 17:32:45 +0530

    ShoppingCart added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 function Header() {
     return (
         <div className="header">
             <img 
                 className="header_logo" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG6.png"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Ksyp"
                 />
         <div className="header_search">
                 <input 
                     className="header_searcbInput"
                     type="text" />
-                    {/* Logo */}
+                    <SearchIcon className="header_searchIcon" />
         </div>
 
         <div className="header_nav">
@@ -41,6 +44,13 @@ function Header() {
                 </span>
                 <span className="header_optionLine2">
                 Prime
+                </span>
+            </div>
+
+            <div className="header_optionB">
+                <ShoppingCartIcon />
+                <span className="header_optionLine2 header_optionB">
+                0
                 </span>
             </div>
         </div>
