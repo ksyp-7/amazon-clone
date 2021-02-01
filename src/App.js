81fe84fd2d0974490af815004d50d78243c6d269
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-01 23:52:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/*Body */}
+      <Home />
     </div>
   );
 }
