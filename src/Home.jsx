b1b3f45a892be4a5f74c7630ba60cbddb46bcc0b
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-02 00:38:31 +0530

    updated

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">         
+            <div className="home_con">
+            <img className="A"
+            src='https://upload.wikimedia.org/wikipedia/commons/3/39/Amazon_Prime_Norge.png'
+            alt=""/>
+            </div>
+        </div>
+    )
+}
+export default Home
