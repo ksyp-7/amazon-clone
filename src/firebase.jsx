8e192f503e33a64f97085feed2608d9b8b20e3a4
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-02 19:15:21 +0530

    Update firebase.jsx

---
+++ src/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCq4r2iq-8OLJhGQV8hjGMQh4AkQ-EhvR8",
+  authDomain: "clone-8dca5.firebaseapp.com",
+  projectId: "clone-8dca5",
+  storageBucket: "clone-8dca5.appspot.com",
+  messagingSenderId: "508690172080",
+  appId: "1:508690172080:web:b557a1c2db86a8ca931375",
+  measurementId: "G-XPW1EWV6D2"
+};
