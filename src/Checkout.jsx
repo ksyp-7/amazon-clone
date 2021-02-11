386e6db59dd0f241620d32879f51e3169621af86
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-11 15:08:55 +0530

    Hello , user(Email)

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -5,7 +5,7 @@ import CP from './CP'
 import {useStateValue} from './StateProvide';
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout">
@@ -15,6 +15,7 @@ function Checkout() {
                     alt=" " 
                     />
                     <div>
+                        <h3>Hello, {user?.email}</h3>
                         <h2 className="c_t">
                         Your Shoping Basket
                         </h2>
