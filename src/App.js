addceb014169402e6737561e6b5f6ed08b30d7c6
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-11 15:07:31 +0530

    Login componet render with SignIn and SignOut working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
+import { auth } from './firebase';
+import {useStateValue} from './StateProvide'
 
 function App() {
+  const [{basket}, dispatch] = useStateValue();
+  useEffect(() => {
+    //will only runs once when the app componet loads....   
+
+    auth.onAuthStateChanged(authUser => {
+      console.log("user >>> ",authUser);
+        if(authUser){
+          // that mens user loged in 
+          dispatch({
+            type: 'SET_USER',
+            user: authUser 
+          })
+
+
+        }else{
+          //that mens user logedout
+          dispatch({
+            type: 'SET_USER',
+            user: null
+          })
+        }
+    })
+  },[])
   return (
     <Router>
       <div className="App">
         <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
